94b9ccb5bb6a4173b4420a8e32fdbcbb4de73a9e
pielover1987@hotmail.com <pielover1987@hotmail.com@9e882c72-dc13-f915-7c92-ac1575c0df7e>
2014-11-25 15:48:29 +0000

    Credit State in correct order.

--- trunk/Trapped Rat/Code/States/CreditsState.cpp
+++ trunk/Trapped Rat/Code/States/CreditsState.cpp
@@ -18,14 +18,23 @@
  {
 	 Font * font = GameData::GetInstance()->GetFont();
 	 font->DrawString("Trapped Rat:", 50.0f, 30.0f, { 155, 155, 155 }, 3.0f);
-	 font->DrawString("Developers...", 50.0f, 80.0f, { 155, 155, 155 });
-	 font->DrawString("Riley Wood, Joe Thompson, Matthew Zanini, Grant Taylor", 100.0f, 110.0f, { 155, 155, 155 });
-	 font->DrawString("Artists...", 50.0f, 140.0f, { 155, 155, 155 });
-	 font->DrawString("Gregory Bey, Caris Frazier", 100.0f, 170.0f, { 155, 155, 155 });
-	 font->DrawString("Executive Producer...", 50.0f, 200.0f, { 155, 155, 155 });
-	 font->DrawString("John Oleske", 100.0f, 230.0f, { 155, 155, 155 });
-	 font->DrawString("Associate Producers...", 50.0f, 260.0f, { 155, 155, 155 });
-	 font->DrawString("Shawn Paris, Robert Martinez", 100.0f, 290.0f, { 155, 155, 155 });
+	 //font->DrawString("Developers...", 50.0f, 80.0f, { 155, 155, 155 });
+	 //font->DrawString("Riley Wood, Joe Thompson, Matthew Zanini, Grant Taylor", 100.0f, 110.0f, { 155, 155, 155 });
+	 //font->DrawString("Artists...", 50.0f, 140.0f, { 155, 155, 155 });
+	 //font->DrawString("Gregory Bey, Caris Frazier", 100.0f, 170.0f, { 155, 155, 155 });
+	 //font->DrawString("Executive Producer...", 50.0f, 200.0f, { 155, 155, 155 });
+	 //font->DrawString("John Oleske", 100.0f, 230.0f, { 155, 155, 155 });
+	 //font->DrawString("Associate Producers...", 50.0f, 260.0f, { 155, 155, 155 });
+	 //font->DrawString("Shawn Paris, Robert Martinez", 100.0f, 290.0f, { 155, 155, 155 });
+	 //font->DrawString("Press Escape to Exit", 50.0f, 500.0f, { 155, 155, 155 });
+	 font->DrawString("Executive Producer...", 50.0f, 80.0f, { 155, 155, 155 });
+	 font->DrawString("John Oleske", 100.0f, 110.0f, { 155, 155, 155 });
+	 font->DrawString("Associate Producers...", 50.0f, 140.0f, { 155, 155, 155 });
+	 font->DrawString("Shawn Paris, Robert Martinez", 100.0f, 170.0f, { 155, 155, 155 });
+	 font->DrawString("Artists...", 50.0f, 200.0f, { 155, 155, 155 });
+	 font->DrawString("Gregory Bey, Caris Frazier", 100.0f, 230.0f, { 155, 155, 155 });
+	 font->DrawString("Developers...", 50.0f, 260.0f, { 155, 155, 155 });
+	 font->DrawString("Riley Wood, Joe Thompson, Matthew Zanini, Grant Taylor", 100.0f, 290.0f, { 155, 155, 155 });
 	 font->DrawString("Press Escape to Exit", 50.0f, 500.0f, { 155, 155, 155 });
  }
 void CreditsState::Enter()
