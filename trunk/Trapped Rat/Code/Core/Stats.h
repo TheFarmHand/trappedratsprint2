f82cc539ae51ab88fadb7863842ad67aab0da590
reiylw@gmail.com <reiylw@gmail.com@9e882c72-dc13-f915-7c92-ac1575c0df7e>
2014-11-25 21:00:25 +0000

    here ya go

--- trunk/Trapped Rat/Code/Core/Stats.h
+++ trunk/Trapped Rat/Code/Core/Stats.h
@@ -6,9 +6,18 @@ public:
 	Stats();
 	~Stats();
 	int attack;
+	float attack_scale;
 	int defense;
+	float defense_scale;
 	int magic;
+	float magic_scale;
 	int avoision;
+
+	//scaling for stuff within the character itself
+	float avoision_scale;
+	float hp_scale;
+	float bp_scale;
+	float speed_scale;
 
 };
 
