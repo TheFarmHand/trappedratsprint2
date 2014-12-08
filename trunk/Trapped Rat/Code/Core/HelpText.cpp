3f04568df7810978b70a460b07e568fdb313dbfe
pielover1987@hotmail.com <pielover1987@hotmail.com@9e882c72-dc13-f915-7c92-ac1575c0df7e>
2014-12-08 19:18:48 +0000

    Help Text added a way to manually override text using the ManualOverride() function

--- trunk/Trapped Rat/Code/Core/HelpText.cpp
+++ trunk/Trapped Rat/Code/Core/HelpText.cpp
@@ -1,9 +1,9 @@
 #include "HelpText.h"
-#include <string>
 #include "GameData.h"
 #include "../Font/Font.h"
 #include "SelectableObject.h"
 #include "Items.h"
+#include "Character.h"
 HelpText::HelpText()
 {
 
@@ -15,7 +15,7 @@ HelpText::~HelpText()
 void HelpText::Render()
 {
 	if (m_backimage != SGD::INVALID_HANDLE)
-		SGD::GraphicsManager::GetInstance()->DrawTexture(m_backimage, {200,0});
+		SGD::GraphicsManager::GetInstance()->DrawTexture(m_backimage, {100,0});
 	std::string str;
 	switch (m_state)
 	{
@@ -40,6 +40,9 @@ void HelpText::Render()
 	case 6:
 		RunDisplay();
 		break;
+	case 7:
+		ManualDisplay();
+		break;
 	}
 }
 //0Home State  1Ability State  2Item State  3Enemy Targetting State    4Ally Targetting State  5Waiting For Turn State  6Run
@@ -54,8 +57,8 @@ void HelpText::HomeDisplay()
 	if (m_selectedObj == nullptr)
 		return;
 	
-	GameData::GetInstance()->GetFont()->DrawString(m_selectedObj->GetExplination(), 216, 16, { 0, 0, 0 }, 1.45f);
-	GameData::GetInstance()->GetFont()->DrawString(m_selectedObj->GetExplination(), 215, 15, {0,0,100},1.45f);
+	GameData::GetInstance()->GetFont()->DrawString(m_selectedObj->GetExplination(), 116, 16, { 0, 0, 0 }, 1.45f);
+	GameData::GetInstance()->GetFont()->DrawString(m_selectedObj->GetExplination(), 115, 15, {0,0,100},1.45f);
 }
 void HelpText::AbilityDisplay()
 {
@@ -63,14 +66,14 @@ void HelpText::AbilityDisplay()
 		return;
 	if (m_selectedObj->GetAbility() == nullptr)
 		return;
-	GameData::GetInstance()->GetFont()->DrawString(std::to_string(m_selectedObj->GetAbility()->GetBPCost()), 216, 16, { 0, 0, 0 }, 1.45f);
-	GameData::GetInstance()->GetFont()->DrawString(std::to_string(m_selectedObj->GetAbility()->GetBPCost()), 215, 15, { 0, 0, 100 }, 1.45f);
+	GameData::GetInstance()->GetFont()->DrawString(std::to_string(m_selectedObj->GetAbility()->GetBPCost()), 116, 16, { 0, 0, 0 }, 1.45f);
+	GameData::GetInstance()->GetFont()->DrawString(std::to_string(m_selectedObj->GetAbility()->GetBPCost()), 115, 15, { 0, 0, 100 }, 1.45f);
 
-	GameData::GetInstance()->GetFont()->DrawString("BP cost", 246, 16, { 0, 0, 0 }, 1.45f);
-	GameData::GetInstance()->GetFont()->DrawString("BP cost", 245, 15, { 0, 0, 100 }, 1.45f);
+	GameData::GetInstance()->GetFont()->DrawString("BP cost", 146, 16, { 0, 0, 0 }, 1.45f);
+	GameData::GetInstance()->GetFont()->DrawString("BP cost", 145, 15, { 0, 0, 100 }, 1.45f);
 
-	GameData::GetInstance()->GetFont()->DrawString(m_selectedObj->GetAbility()->GetExplination(), 216, 16, { 0, 0, 0 }, 1.45f);
-	GameData::GetInstance()->GetFont()->DrawString(m_selectedObj->GetAbility()->GetExplination(), 215, 15, { 0, 0, 100 }, 1.45f);
+	GameData::GetInstance()->GetFont()->DrawString(m_selectedObj->GetAbility()->GetExplination(), 116, 16, { 0, 0, 0 }, 1.45f);
+	GameData::GetInstance()->GetFont()->DrawString(m_selectedObj->GetAbility()->GetExplination(), 115, 15, { 0, 0, 100 }, 1.45f);
 }
 void HelpText::ItemDisplay()
 {
@@ -79,26 +82,60 @@ void HelpText::ItemDisplay()
 	if (m_selectedObj->GetItem() == nullptr)
 		return;
 
-	GameData::GetInstance()->GetFont()->DrawString(m_selectedObj->GetItem()->GetExplination(), 216, 16, { 0, 0, 0 }, 1.45f);
-	GameData::GetInstance()->GetFont()->DrawString(m_selectedObj->GetItem()->GetExplination(), 215, 15, { 0, 0, 100 }, 1.45f);
+	GameData::GetInstance()->GetFont()->DrawString(m_selectedObj->GetItem()->GetExplination(), 116, 16, { 0, 0, 0 }, 1.45f);
+	GameData::GetInstance()->GetFont()->DrawString(m_selectedObj->GetItem()->GetExplination(), 115, 15, { 0, 0, 100 }, 1.45f);
 }
 void HelpText::EnemyDisplay()
 {
-	GameData::GetInstance()->GetFont()->DrawString("Select an ENEMY for your _attack or ability", 216, 16, { 0, 0, 0 }, 1.45f);
-	GameData::GetInstance()->GetFont()->DrawString("Select an ENEMY for your _attack or ability", 215, 15, { 0, 0, 100 }, 1.45f);
+	GameData::GetInstance()->GetFont()->DrawString("Select an ENEMY for your _attack or ability", 116, 16, { 0, 0, 0 }, 1.45f);
+	GameData::GetInstance()->GetFont()->DrawString("Select an ENEMY for your _attack or ability", 115, 15, { 0, 0, 100 }, 1.45f);
 }
 void HelpText::AllyDisplay()
 {
-	GameData::GetInstance()->GetFont()->DrawString("Select an ALLY for your _item or ability", 216, 16, { 0, 0, 0 }, 1.45f);
-	GameData::GetInstance()->GetFont()->DrawString("Select an ALLY for your _item or ability", 215, 15, { 0, 0, 100 }, 1.45f);
+	GameData::GetInstance()->GetFont()->DrawString("Select an ALLY for your _item or ability", 116, 16, { 0, 0, 0 }, 1.45f);
+	GameData::GetInstance()->GetFont()->DrawString("Select an ALLY for your _item or ability", 115, 15, { 0, 0, 100 }, 1.45f);
 }
 void HelpText::WaitingDisplay()
 {
-	GameData::GetInstance()->GetFont()->DrawString("You are now planning your next move", 216, 16, { 0, 0, 0 }, 1.45f);
-	GameData::GetInstance()->GetFont()->DrawString("You are now planning your next move", 215, 15, {0,0,100},1.45f);
+	GameData::GetInstance()->GetFont()->DrawString("You are now planning your next move", 116, 16, { 0, 0, 0 }, 1.45f);
+	GameData::GetInstance()->GetFont()->DrawString("You are now planning your next move", 115, 15, {0,0,100},1.45f);
 }
 void HelpText::RunDisplay()
 {
-	GameData::GetInstance()->GetFont()->DrawString("Select yourself to confirm _your COWARDICE", 216, 16, { 0, 0, 0 }, 1.45f);
-	GameData::GetInstance()->GetFont()->DrawString("Select yourself to confirm _your COWARDICE", 215, 15, { 0, 0, 100 }, 1.45f);
+	GameData::GetInstance()->GetFont()->DrawString("Select yourself to confirm _your COWARDICE", 116, 16, { 0, 0, 0 }, 1.45f);
+	GameData::GetInstance()->GetFont()->DrawString("Select yourself to confirm _your COWARDICE", 115, 15, { 0, 0, 100 }, 1.45f);
+}
+
+void HelpText::ManualOverride(std::string _customstring, Character* _ownercharacter)
+{
+	m_state = 7;
+	m_overridestring = _customstring;
+	m_activeCharacter = _ownercharacter;
+}
+void HelpText::ManualDisplay()
+{
+
+	//Render Portrait
+	
+	if (m_activeCharacter != nullptr)
+	{
+		if (m_activeCharacter->GetPortrait() != SGD::INVALID_HANDLE)
+			{
+			if (m_activeCharacter->GetAlliance() == "Ally")
+			{
+				SGD::GraphicsManager::GetInstance()->DrawTexture(m_activeCharacter->GetPortrait(), { 110, 10 });
+				GameData::GetInstance()->GetFont()->DrawString(m_overridestring, 180, 16, { 0, 0, 0 }, 1.45f);
+				GameData::GetInstance()->GetFont()->DrawString(m_overridestring, 179, 15, { 0, 0, 100 }, 1.45f);
+			}
+			else
+			{
+				SGD::GraphicsManager::GetInstance()->DrawTexture(m_activeCharacter->GetPortrait(), {606,10});
+				GameData::GetInstance()->GetFont()->DrawString(m_overridestring, 116, 16, { 0, 0, 0 }, 1.45f);
+				GameData::GetInstance()->GetFont()->DrawString(m_overridestring, 115, 15, { 0, 0, 100 }, 1.45f);
+			}
+			}
+	}
+
+
+
 }
--- trunk/Trapped Rat/Code/Core/HelpText.h
+++ trunk/Trapped Rat/Code/Core/HelpText.h
@@ -1,13 +1,17 @@
 #pragma once
 #include "../SGD Wrappers/SGD_GraphicsManager.h"
-
+#include <string>
 class SelectableObject;
+class Character;
 class HelpText
 {
 private:
-	int m_state = 5; //0Home State  1Ability State  2Item State  3Enemy Targetting State    4Ally Targetting State  5Waiting For Turn State 6 Run
+	int m_state = 5; //0Home State  1Ability State  2Item State  3Enemy Targetting State    4Ally Targetting State  5Waiting For Turn State 6 Run  7 Manual Override
 	SGD::HTexture m_backimage = SGD::INVALID_HANDLE;
 	SelectableObject* m_selectedObj = nullptr;
+	Character*		  m_activeCharacter = nullptr;
+	std::string  m_overridestring;
+
 
 	void HomeDisplay();
 	void AbilityDisplay();
@@ -16,12 +20,15 @@ private:
 	void AllyDisplay();
 	void WaitingDisplay();
 	void RunDisplay();
+	void ManualDisplay();
 
 public:
 	HelpText();
 	~HelpText();
 	void Render();
-	//0Home State  1Ability State  2Item State  3Enemy Targetting State    4Ally Targetting State  5Waiting For Turn State 6 Run
+	//0Home State  1Ability State  2Item State  3Enemy Targetting State    4Ally Targetting State  5Waiting For Turn State 6 Run   7 Manual Override
 	void UpdateSelection(int currentState,SelectableObject* obj = nullptr);
 	void SetImage(SGD::HTexture img){ m_backimage = img; }
+	//String that will be displayed in the help text, A character pointer to obtain portrait information
+	void ManualOverride(std::string _customstring, Character* _ownercharacter = nullptr);
 };
